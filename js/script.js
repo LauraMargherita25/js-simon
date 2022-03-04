cf8a7f509bc21958e0494a170d09c59b68b38a21
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-04 16:00:52 +0100

    5 rnd num

--- js/script.js
+++ js/script.js
@@ -5,10 +5,22 @@
 // confronto i numeri inseriti dall'utente con quelli rnd( confronto tra array?)
 
 
-// creo numeri rnd 
-let rndNum = getRndNum(1, 100);
-console.log(rndNum);
+// creo numero rnd 
+let rndNum1 = getRndNum1(1, 100);
+console.log("sono rndNum1" + " " + rndNum1);
 
-function getRndNum(min, max) {
+function getRndNum1(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// creo 5 rndNum
+
+for (let i = 0; i < 5; i++) {
+    
+    let rndNum5 = getRndNum5(1, 100);
+    console.log("siamo rndNum5" + " " + rndNum5);
+
+    function getRndNum5(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
 }
