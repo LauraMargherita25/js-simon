5ab2d15953a02aea4e3cdbb596f2ed262976f367
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-04 16:16:14 +0100

    Push rnd num in arrRndNum

--- js/script.js
+++ js/script.js
@@ -5,22 +5,19 @@
 // confronto i numeri inseriti dall'utente con quelli rnd( confronto tra array?)
 
 
-// creo numero rnd 
-let rndNum1 = getRndNum1(1, 100);
-console.log("sono rndNum1" + " " + rndNum1);
-
-function getRndNum1(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
 // creo 5 rndNum
+
+let arrRndNum = []
 
 for (let i = 0; i < 5; i++) {
     
-    let rndNum5 = getRndNum5(1, 100);
-    console.log("siamo rndNum5" + " " + rndNum5);
-
-    function getRndNum5(min, max) {
+    let rndNum = getRndNum(1, 100);
+    console.log("siamo 5 rndNum" + " " + rndNum);
+    
+    function getRndNum(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
+    
+    arrRndNum.push(rndNum);
 }
+console.log(arrRndNum);
