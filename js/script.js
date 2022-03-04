0f391a0e5becac2fc2c0c32fbbad94228397994c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-04 15:29:16 +0100

    Flowchart

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//creo 5 numeri casuali
+// da visualizzare nella pagina
+// faccio partire un timer(count down)all'apertura della pagina che fa sparire i numeri dopo 30 sec
+// qundo i numeri scompaiono chiedo con prompt i numeri uno alla volta(5 prompt diversi?)
+// confronto i numeri inseriti dall'utente con quelli rnd( confronto tra array?)
