bf7699f17ae4dddc797f829fc23752afd60ec1ff
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-04 15:52:02 +0100

    Trying to create 5 rndnum with a function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // faccio partire un timer(count down)all'apertura della pagina che fa sparire i numeri dopo 30 sec
 // qundo i numeri scompaiono chiedo con prompt i numeri uno alla volta(5 prompt diversi?)
 // confronto i numeri inseriti dall'utente con quelli rnd( confronto tra array?)
+
+
+// creo cinque numeri rnd 
+let rndNum = (1, 100);
+console.log(rndNum);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
